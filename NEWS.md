3cc2d2a0aa5c4d0e1d4d824968b15d118a9124d7
Maxime Kuntz <maxime.kuntz75@gmail.com>
2024-04-16 20:50:07 +0000

    version 0.1.2

---
+++ NEWS.md
@@ -0,0 +1,12 @@
+# Paris2024Colours 0.1.2
+
+* Description fix for CRAN submission
+
+# Paris2024Colours 0.1.1
+
+* Documentation fixes for CRAN submission
+
+# Paris2024Colours 0.1.0
+
+* Initial CRAN submission.
+* Colour palettes: Logo, OlympicRings and GamesLook.
---
+++ R/colours.R
@@ -0,0 +1,10 @@
+#' Complete list of palettes
+#'
+#' Use \code{\link{paris2024_palette}} to construct palettes of desired length.
+#'
+#' @export
+paris2024_palettes <- list(
+  Logo = c("#D7C378", "#000000"),
+  OlympicRings = c("#0078D0", "#FFB114", "#000000", "#00A651", "#F0282D"),
+  GamesLook = c("#F1ABC9", "#D9C47A", "#2393CD", "#003561")
+)
